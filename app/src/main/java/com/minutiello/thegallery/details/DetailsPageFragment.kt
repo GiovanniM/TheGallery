0e666a2e5b7963ee80737ebba63b37063136ef9b
Giovanni Minutiello <giovanni.minutiello@gmail.com>
2020-08-30 08:56:00 +0200

    Add image info to full screen view
    Info taken from json, like title, author, creation date and score, are shown in the image full screen page.
    With a tap on the screen these info are hidden/shown to allow the user to enjoy the image in full screen

--- app/src/main/java/com/minutiello/thegallery/details/DetailsPageFragment.kt
+++ app/src/main/java/com/minutiello/thegallery/details/DetailsPageFragment.kt
@@ -8,6 +8,7 @@ import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.minutiello.thegallery.R
 import com.minutiello.thegallery.databinding.DetailsPageFragmentBinding
 import com.minutiello.thegallery.redditrepository.RedditImage
+import java.util.*
 
 private const val ARG_IMAGE = "ARG_IMAGE"
 
@@ -15,6 +16,8 @@ class DetailsPageFragment : Fragment() {
 
     private var _binding: DetailsPageFragmentBinding? = null
     private val binding get() = _binding!!
+
+    private var showingInfo = true
 
     companion object {
         fun newInstance(redditImage: RedditImage): Fragment {
@@ -38,6 +41,12 @@ class DetailsPageFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val redditImage: RedditImage? = arguments?.getSerializable(ARG_IMAGE) as RedditImage?
+        loadImage(redditImage)
+        loadInfo(redditImage)
+        initClickListener()
+    }
+
+    private fun loadImage(redditImage: RedditImage?) {
         Glide.with(this)
             .load(redditImage?.fullUrl)
             .centerInside()
@@ -47,6 +56,35 @@ class DetailsPageFragment : Fragment() {
             .into(binding.fullImageView)
     }
 
+    private fun loadInfo(redditImage: RedditImage?) {
+        redditImage?.run {
+            val unknown = getString(R.string.unknown)
+            val date: String = if (created != null) {
+                Date(created * 1000).toString()
+            } else {
+                unknown
+            }
+            binding.imageTitleAuthorTextView.text =
+                getString(R.string.title_and_author, title ?: unknown, author ?: unknown)
+            binding.imageDateLikesTextView.text =
+                getString(R.string.creation_and_likes, date, score ?: 0)
+        }
+    }
+
+    private fun initClickListener() {
+        binding.fullImageView.setOnClickListener {
+            if (showingInfo) {
+                binding.imageTitleAuthorTextView.animate()
+                    .translationY(-binding.imageTitleAuthorTextView.measuredHeight.toFloat())
+                binding.imageDateLikesTextView.animate()
+                    .translationY(binding.imageDateLikesTextView.measuredHeight.toFloat())
+            } else {
+                binding.imageTitleAuthorTextView.animate().translationY(0f)
+                binding.imageDateLikesTextView.animate().translationY(0f)
+            }
+            showingInfo = !showingInfo
+        }
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
